cfceeb4fa75f983bf38e5de39e09f687eb02459e
Эрик Замабувараев‐Ёмолкуу <erik@freebasic.su>
2023-07-07 17:43:06 +0700

    Удаление неиспользуемых функций

--- dependencies.mk
+++ dependencies.mk
@@ -1,7 +1,5 @@
 $(OBJ_DEBUG_DIR)$(PATH_SEP)AsyncResult$(FILE_SUFFIX).c: src$(PATH_SEP)AsyncResult.bi src$(PATH_SEP)IAsyncResult.bi src$(PATH_SEP)ContainerOf.bi
 $(OBJ_RELEASE_DIR)$(PATH_SEP)AsyncResult$(FILE_SUFFIX).c: src$(PATH_SEP)AsyncResult.bi src$(PATH_SEP)IAsyncResult.bi src$(PATH_SEP)ContainerOf.bi
-$(OBJ_DEBUG_DIR)$(PATH_SEP)DisplayError$(FILE_SUFFIX).c: src$(PATH_SEP)DisplayError.bi
-$(OBJ_RELEASE_DIR)$(PATH_SEP)DisplayError$(FILE_SUFFIX).c: src$(PATH_SEP)DisplayError.bi
 $(OBJ_DEBUG_DIR)$(PATH_SEP)Guids$(FILE_SUFFIX).c: 
 $(OBJ_RELEASE_DIR)$(PATH_SEP)Guids$(FILE_SUFFIX).c: 
 $(OBJ_DEBUG_DIR)$(PATH_SEP)Resources$(FILE_SUFFIX).obj: src$(PATH_SEP)Resources.RH src$(PATH_SEP)app.exe.manifest
